63d7bc5484b85ee4b85e6dd47c0a5fb5d77fe97a
Sefaal <spotvtec@gmail.com>
2020-11-22 13:29:50 -0500

    Reading output from two probes

--- test_3_relay.py
+++ test_3_relay.py
@@ -14,35 +14,32 @@ ads = ADS.ADS1115(i2c)
 # ads = ADS.ADS1115(i2c, address=0x49)
 
 # Create single-ended input on channel 0
-chan = AnalogIn(ads, ADS.P0)
-
-chan = AnalogIn(ads, ADS.P1)
+chan1 = AnalogIn(ads, ADS.P0)
+chan2 = AnalogIn(ads, ADS.P1)
 
 # Create differential input between channel 0 and 1
 # chan = AnalogIn(ads, ADS.P0, ADS.P1)
 
-relay = OutputDevice(17, active_high=False, initial_value=False)
-
-print("{:>5}\t{:>5}".format("raw", "v"))
-
-while True:
-  sleep(5)
-  if (chan.voltage < 2.0):
-    print("Off: {:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
-    relay.off()
-  else:
-    print("On: {:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
-    relay.on()
-
-#relay2 = OutputDevice(27, active_high=False, initial_value=False)
-
+relay1 = OutputDevice(17, active_high=False, initial_value=False)
 relay2 = OutputDevice(27, active_high=False, initial_value=False)
 
+print("Chan 1:\t{:>5}\t{:>5}\t\tChan 2:\t{:>5}\t{:>5}".format("raw", "v", "raw", "v"))
+
 while True:
-  sleep(5)
-  if (chan.voltage < 2.0):
-    print("Off: {:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
-    relay.off()
+  sleep(1)
+
+  if (chan1.voltage < 1.9):
+    chan1result = "P1-Off:\t{:>5}\t{:>5.3f}".format(chan1.value, chan1.voltage)
+    relay1.off()
   else:
-    print("On: {:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
-    relay.on()
+    chan1result = "P1-On:\t{:>5}\t{:>5.3f}".format(chan1.value, chan1.voltage)
+    relay1.on()
+
+  if (chan2.voltage < 1.9):
+    chan2result = "\t\tP2-Off:\t{:>5}\t{:>5.3f}".format(chan2.value, chan2.voltage)
+    relay2.off()
+  else:
+    chan2result = "\t\tP2-On:\t{:>5}\t{:>5.3f}".format(chan2.value, chan2.voltage)
+    relay2.on()
+
+  print(chan1result + chan2result)
