9ab893fe6da5c2db3f65b16555614ebfe09a4dde
Sefaal <spotvtec@gmail.com>
2020-11-22 20:45:41 -0500

    WIP: Irrigation scheduling

--- irrigationAutomator.py
+++ irrigationAutomator.py
@@ -4,6 +4,25 @@ import board
 import busio
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
+import datetime
+from datetime import datetime, date, time, timedelta
+
+#datetime1 = datetime.min
+#print(datetime1)
+
+#sleep(5)
+
+#datetime2 = datetime.now()
+#print(datetime2)
+
+# elapsed is a timedelta object
+#elapsed = datetime2 - datetime1
+#print(elapsed)
+
+#print(isinstance(elapsed, timedelta))
+
+#print(elapsed.seconds)
+
 
 # Create the I2C bus
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -32,28 +51,58 @@ probe2wet = 1.180
 probe2dry = 2.330
 probe2range = probe2dry - probe2wet
 
-pumpToggleRatio = 0.7
-
 #print("probe1range result: {:>5.3f}".format(probe1range))
 #print("probe2range result: {:>5.3f}".format(probe2range))
+
+
+# Constants
+
+pumpToggleThreshold = 0.7
+
+maxWaterPumpingPerDayInSeconds = 60
+maxWaterPumpingPerActivationInSeconds = 15
+maxPumpActivationsPerDay = int(maxWaterPumpingPerDayInSeconds / maxWaterPumpingPerActivationInSeconds)
+
+print(maxPumpActivationsPerDay)
+
+postWaterDelayCheck = 60
+
+
+
+
+
+# Variables
+waterTimingsPump1 = []
+waterTimingsPump2 = []
 
 
 print("P1:\t{:>5}\t{:>5}\t{:>5}\t\tP2:\t{:>5}\t{:>5}\t{:>5}".format("raw", "v", "%", "raw", "v", "%"))
 
 while True:
+
+
+
+
+  if (len(waterTimingsPump1) < maxPumpActivationsPerDay):
+    print(len(waterTimingsPump1))
+
+#  if (probe1MoistureRatio > pumpToggleRatio):
+
+
+
   sleep(1)
 
   probe1MoistureRatio = (chan1.voltage - probe1wet) / probe1range
   probe2MoistureRatio = (chan2.voltage - probe2wet) / probe2range
 
-  if (probe1MoistureRatio < pumpToggleRatio):
+  if (probe1MoistureRatio < pumpToggleThreshold):
     chan1result = "P1-Off:\t{:>5}\t{:>5.3f}\t{:>5.3f}".format(chan1.value, chan1.voltage, probe1MoistureRatio)
     relay1.off()
   else:
     chan1result = "P1-On:\t{:>5}\t{:>5.3f}\t{:>5.3f}".format(chan1.value, chan1.voltage, probe1MoistureRatio)
     relay1.on()
 
-  if (probe2MoistureRatio < pumpToggleRatio):
+  if (probe2MoistureRatio < pumpToggleThreshold):
     chan2result = "P2-Off:\t{:>5}\t{:>5.3f}\t{:>5.3f}".format(chan2.value, chan2.voltage, probe2MoistureRatio)
     relay2.off()
   else:
